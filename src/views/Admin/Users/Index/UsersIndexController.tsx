fd889609bb89d8032cdd323b2d5f4a06f8717582
codenoobies <kevindubidaam@gmail.com>
2024-07-15 22:26:36 +0700

    add (routes): added catch all and redirect into admin route

--- src/views/Admin/Users/Index/UsersIndexController.tsx
+++ src/views/Admin/Users/Index/UsersIndexController.tsx
@@ -11,109 +11,106 @@ import { PermissionsEnum } from "@/lib/enum/PermissionsEnum";
 import { checkPermissions } from "@/lib/utils/CheckPermission";
 
 function useUsersIndexController() {
-  const {
-    isUserFetching,
-    refetchUser,
-    setUserQuery,
-    userQuery,
-    usersData,
-    mutateDeleteUser,
-  } = useUsersIndexModel();
+	const {
+		isUserFetching,
+		refetchUser,
+		setUserQuery,
+		userQuery,
+		usersData,
+		mutateDeleteUser,
+	} = useUsersIndexModel();
 
-  const handleUserSearch = (value: string) => {
-    setUserQuery((prev) => ({ ...prev, name: value }));
-  };
+	const handleUserSearch = (value: string) => {
+		setUserQuery((prev) => ({ ...prev, name: value }));
+	};
 
-  const handlePageChange = (page: number) => {
-    setUserQuery((prev) => ({ ...prev, page }));
-  };
+	const handlePageChange = (page: number) => {
+		setUserQuery((prev) => ({ ...prev, page }));
+	};
 
-  const handleDeleteUser = (id: string) => {
-    mutateDeleteUser({ id });
-  };
+	const handleDeleteUser = (id: string) => {
+		mutateDeleteUser({ id });
+	};
 
-  const navigate = useNavigate();
+	const navigate = useNavigate();
 
-  const tableColumns: DataTableColumn<UserModel>[] = [
-    {
-      accessor: "actions",
-      title: "Actions",
-      textAlign: "center",
-      render(record) {
-        return (
-          <div className="flex gap-3 justify-center">
-            <ActionIcon
-              variant="light"
-              color="orange"
-              disabled={
-                record.role_id.role_name === "Super Admin" ||
-                !checkPermissions({
-                  permissionsCode: PermissionsEnum.UPDATE_USER,
-                  type: "action",
-                })
-              }
-              onClick={() => navigate(`./${record._id}/edit`)}
-            >
-              <IconEdit />
-            </ActionIcon>
-            <Popconfirm
-              description="Yakin ingin menghapus data ini ?"
-              onConfirm={() => handleDeleteUser(record._id)}
-            >
-              <ActionIcon
-                variant="light"
-                color="red"
-                disabled={
-                  record.role_id.role_name === "Super Admin" ||
-                  !checkPermissions({
-                    permissionsCode: PermissionsEnum.DELETE_USER,
-                    type: "action",
-                  })
-                }
-              >
-                <IconTrash />
-              </ActionIcon>
-            </Popconfirm>
-          </div>
-        );
-      },
-    },
-    {
-      accessor: "name",
-      title: "Nama User",
-    },
-    {
-      accessor: "username",
-      title: "Username",
-    },
-    {
-      accessor: "role_id.role_name",
-      title: "Role",
-      render(record) {
-        return record.role_id.role_name;
-      },
-    },
-    {
-      accessor: "date_created",
-      title: "Tanggal Dibuat",
-      render(record) {
-        return dayjs(record.date_created).format("DD MMM YYYY");
-      },
-    },
-  ];
+	const tableColumns: DataTableColumn<UserModel>[] = [
+		{
+			accessor: "actions",
+			title: "Actions",
+			textAlign: "center",
+			render(record) {
+				return (
+					<div className="flex gap-3 justify-center">
+						<ActionIcon
+							variant="light"
+							color="orange"
+							disabled={
+								record?.role_id?.role_name === "Super Admin" ||
+								!checkPermissions({
+									permissionsCode: PermissionsEnum.UPDATE_USER,
+									type: "action",
+								})
+							}
+							onClick={() => navigate(`./${record._id}/edit`)}>
+							<IconEdit />
+						</ActionIcon>
+						<Popconfirm
+							description="Yakin ingin menghapus data ini ?"
+							onConfirm={() => handleDeleteUser(record._id)}>
+							<ActionIcon
+								variant="light"
+								color="red"
+								disabled={
+									record?.role_id?.role_name === "Super Admin" ||
+									!checkPermissions({
+										permissionsCode: PermissionsEnum.DELETE_USER,
+										type: "action",
+									})
+								}>
+								<IconTrash />
+							</ActionIcon>
+						</Popconfirm>
+					</div>
+				);
+			},
+		},
+		{
+			accessor: "name",
+			title: "Nama User",
+		},
+		{
+			accessor: "username",
+			title: "Username",
+		},
+		{
+			accessor: "role_id.role_name",
+			title: "Role",
+			render(record) {
+				return record?.role_id?.role_name;
+			},
+		},
+		{
+			accessor: "date_created",
+			title: "Tanggal Dibuat",
+			render(record) {
+				return dayjs(record.date_created).format("DD MMM YYYY");
+			},
+		},
+	];
 
-  usePageTitle({ title: "Users" });
+	usePageTitle({ title: "Users" });
 
-  return {
-    isUserFetching,
-    refetchUser,
-    userQuery,
-    usersData,
-    handleUserSearch,
-    handlePageChange,
-    tableColumns,
-    navigate,
-  };
+	return {
+		isUserFetching,
+		refetchUser,
+		userQuery,
+		usersData,
+		handleUserSearch,
+		handlePageChange,
+		tableColumns,
+		navigate,
+	};
 }
 
 export default useUsersIndexController;
